af3dacc09dbb4755d9a161e11149c3d2e2360d3f
Omm Prakash Jena <ojena643@gmail.com>
2025-10-08 21:26:19 +0530

    Add solution for Problem 43: Conceptual discussion on differences between interface and abstract class

---
+++ Problem43.java
@@ -0,0 +1,12 @@
+// Problem 43: Discuss the difference between interface and abstract class in terms of multiple inheritance, fields, and constructors. (Conceptual, in comments).
+
+public class Problem43 {
+    // Interface vs Abstract Class:
+    // 1. Multiple Inheritance: A class can implement multiple interfaces, but can extend only one abstract class.
+    // 2. Fields: Interfaces can have only public static final fields (constants). Abstract classes can have instance variables.
+    // 3. Constructors: Interfaces cannot have constructors. Abstract classes can have constructors.
+
+    public static void main(String[] args) {
+        System.out.println("Conceptual discussion in comments");
+    }
+}
